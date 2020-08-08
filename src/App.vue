d3f3247a863370d5c8bc70d85d993fcc717b2190
Nemo Andrea <nemoandrea@gmail.com>
2020-08-08 14:02:07 +0200

    Minimal Vue + Vuetify code
    adding the minimal functional version of vue and vuetify

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-main>
+      Apples and oranges
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+  ,
+
+  data: () => ({
+    //
+  }),
+};
+</script>
