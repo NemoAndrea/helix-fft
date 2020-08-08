2a2f180f087419812a0587c051fa2cd71482d056
Nemo Andrea <nemoandrea@gmail.com>
2020-08-09 00:15:22 +0200

    Three.js realspace helix
    Added three.js library for drawing the realspace images of the helices. Some basic autoscaling features for the helix.

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,73 @@
         <div class="layout-left"><div class="rotated">Built by Nemo Andrea</div></div>
         <div class="layout-central">
           <div class="ui-box ui-command-panel">
-            <div class="card card-accent">Metadata card |
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-              ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-              mollit anim id est laborum</div>
-            <div class="card">Helix parameters</div>
+            <div class="card card-accent">
+              <div class="card-title">Metadata card</div>
+              <v-btn disabled>Load example</v-btn>
+            </div>
+            <div class="card">
+              <div class="card-title">Helix parameters</div>
+              <v-form
+                      ref="form"
+                      v-model="valid">
+
+                <v-text-field
+                        v-model="radius"
+                        :rules="numberRules"
+                        label="Radius"
+                        required
+                        filled
+                        color="var(--primary)"
+                        suffix="nm"
+                        type="number"
+                ></v-text-field>
+
+                <v-text-field
+                        v-model="rise"
+                        :rules="numberRules"
+                        label="Rise"
+                        required
+                        filled
+                        color="var(--primary)"
+                        suffix="nm"
+                        type="number"
+                ></v-text-field>
+
+                <v-text-field
+                        v-model="frequency"
+                        :rules="numberRules"
+                        label="Frequency"
+                        required
+                        filled
+                        color="var(--primary)"
+                        suffix="nm"
+                        type="number"
+                ></v-text-field>
+
+                <v-text-field
+                        v-model="unit_size"
+                        :rules="numberRules"
+                        label="Unit size"
+                        required
+                        filled
+                        color="var(--primary)"
+                        suffix="nm"
+                        type="number"
+                ></v-text-field>
+
+                <v-btn
+                        :disabled="!valid"
+                        color="var(--primary)"
+                        @click="computeHelix">
+                  Compute
+                </v-btn>
+
+              </v-form>
+            </div>
           </div>
-          <div class="ui-box ui-realspace-panel"><div class="card card-webgl">Realspace helix</div></div>
+          <div class="ui-box ui-realspace-panel"><div class="card card-webgl">
+            <div class="card-title-float card-title">Realspace helix</div>
+          </div></div>
           <div class="ui-box ui-fft-panel">
             <div class="card card-display">Display Controls</div>
             <div class="card card-fft">FFT of helix (theory and computed)</div>
@@ -28,14 +86,80 @@
 </template>
 
 <script>
+import * as THREE from '../ext/three.module.js';
 
 export default {
   name: 'App'
   ,
 
   data: () => ({
-    //
+    valid: false,
+    radius: "",
+    rise: "",
+    frequency: "",
+    unit_size: "",
+    numberRules: [v => !!v || 'parameter is required',],
   }),
+  methods: {
+    computeHelix() {
+      console.log('Helix parameters: ' + this.radius + this.rise + this.frequency + this.unit_size);
+      const scalefac = 10/this.radius;  // the prefactor is an empirical value and might need to be made more robust
+
+      // get the div we will draw in
+      let container = document.querySelector('.card-webgl' );
+      let containerinfo = container.getBoundingClientRect();
+
+      // set up the camera
+      THREE.Object3D.DefaultUp = new THREE.Vector3( 0,0,1);
+      let camera = new THREE.PerspectiveCamera( 40, containerinfo.width/containerinfo.height, 0.1, 15000 );
+      camera.position.z = 50;
+      const cam_radius = 160;
+      camera.position.x = cam_radius;
+      let cam_angle = 0;  // initialise to 0
+      camera.lookAt( new THREE.Vector3(0,0,50) );
+
+      let scene = new THREE.Scene();
+
+      // set up helical building blocks
+      let geometry = new THREE.SphereGeometry( this.unit_size*scalefac, 12,12);
+      let material = new THREE.MeshNormalMaterial();
+
+      const amount = Math.ceil(100 / (this.rise*scalefac));
+
+      // build the helix
+      for ( var i = 0; i < amount ; i ++ ) {
+        let object = new THREE.Mesh( geometry, material );
+        object.position.x = this.radius*scalefac*Math.cos(i*2*Math.PI/this.frequency);
+        object.position.y = this.radius*scalefac*Math.sin(i*2*Math.PI/this.frequency);
+        object.position.z = i*this.rise*scalefac;
+
+
+        scene.add( object );
+      }
+
+      // setup render window
+      let renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true} );
+      renderer.setPixelRatio( window.devicePixelRatio );
+      renderer.setSize( containerinfo.width, containerinfo.height );
+      container.appendChild( renderer.domElement );
+
+      // function that is repeatedly automatically called
+      function animate() {
+        requestAnimationFrame( animate );
+        render();
+      }
+      function render(){
+        camera.position.x = cam_radius*Math.cos(cam_angle);
+        camera.position.y = cam_radius*Math.sin(cam_angle);
+        cam_angle = cam_angle + 0.01;
+
+        camera.lookAt( new THREE.Vector3(0,0,50) );
+        renderer.render( scene, camera );
+
+      }
+      animate();
+    },
+  }
 };
 </script>
 
@@ -54,6 +178,7 @@ export default {
     grid-template: auto 1fr auto / auto 1fr auto;
     width: 100vw;
     height: 100vh;
+    overflow-y: scroll;
   }
 
   header{
@@ -126,6 +251,18 @@ export default {
     border-radius: 0 30px 30px 30px;
   }
 
+  .card-title{
+    font-size: 1.3rem;
+    margin-bottom: 0.5rem;
+  }
+
+  .card-title-float{
+    margin: 1rem;
+    padding: 0.6rem;
+    position: absolute;
+    font-weight: bold;
+  }
+
   .card-accent{
     background-color: var(--primary);
     margin-bottom: 1rem;
@@ -135,6 +272,8 @@ export default {
   .card-webgl{
     border-radius: 7px 7px 7px 7px;
     height: 100%;
+    padding: 0;
+    position: relative;
   }
 
   .card-display{
