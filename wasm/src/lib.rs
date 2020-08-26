dadfe4f43e3880027acb191fe23c729a730db338
Nemo Andrea <nemoandrea@gmail.com>
2020-08-26 02:04:53 +0200

    Fixed error in analytic diffraction
    Fixed error in treatment of helix offset (along z). Slightly increases computational cost of calculation. Verified to work properly based on microtubule diffraction pattern

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -91,13 +91,13 @@ pub fn FFT_analytic( helix_family: &JsValue, n_range: u8, m_range: u8, scale: f6
     // helix_family is an array of dicts in JS.
     let helices: Vec<Element> = helix_family.into_serde().unwrap();
 
-    let mut m_vals: Vec<i8> = vec![];
+    let mut m_vals: Vec<f64> = vec![];
     for i in 0..m_range {
-        if i == 0{ m_vals.push(i as i8) }
+        if i == 0{ m_vals.push(i as f64) }
         else {
             // add +m and -m
-            m_vals.push( (i as i8));
-            m_vals.push(-(i as i8));
+            m_vals.push( (i as f64));
+            m_vals.push(-(i as f64));
         }
     };
 
@@ -107,45 +107,56 @@ pub fn FFT_analytic( helix_family: &JsValue, n_range: u8, m_range: u8, scale: f6
     // get a set of coordinates [-rastersize/2 to rastersize/2]
     let coords:Array1<f64> = Array::range(0.,raster_size as f64, 1 as f64) - (raster_size / 2) as f64;
 
-    let mut z_line:f64;
-    let mut bessel:f64;
+    let mut z_line:f64;  // the layerline coordinate
+    let mut z_draw_coord:f64;  // rounded value of z_line (for drawing in image)
+    let mut bessel:Array1<f64> = Array::zeros(raster_size as usize);
     let mut Ufac:Array1<Complex64> = Array::zeros(raster_size as usize);
 
+    let mut phi_j:f64;  // phase change due to angular offset helix
+    let mut phi_z:f64;  // phase change due to offset along helix axis (z)
+
     //loop over different helices
-    for helix in helices {
+    for helix in &helices {
         // first get helix info into a datatype we can handle
         let radius:f64 = helix.radius.parse::<f64>().unwrap_or(0 as f64);
         let rise:f64 = helix.rise.parse::<f64>().unwrap_or(0 as f64);
         let frequency:f64 = helix.frequency.parse::<f64>().unwrap_or(0 as f64);
-        //let unit_size:f64 = helix.unit_size.parse::<f64>().unwrap_or(0 as f64);  // not used atm
+        //let unit_size:f64 = helix.unit_size.parse::<f64>().unwrap_or(0 as f64);  // not used currently
         let offset:f64 = helix.offset.parse::<f64>().unwrap_or(0 as f64);
         let rotation:f64 = helix.rotation.parse::<f64>().unwrap_or(0 as f64);
-        //let handedness:String = helix.handedness.parse::<String>().unwrap_or("right".clone().parse().unwrap()); // not used atm
+        //let handedness:String = helix.handedness.parse::<String>().unwrap_or("right".clone().parse().unwrap());  // not used currently
 
         // compute some useful quantities
         let pitch:f64 = rise * frequency;
-        let phi_j:f64 = offset / pitch * 2. * PI + rotation * PI/180.; // rotation due to offset AND the rotation
+        phi_j = rotation * PI/180.; // rotation due to offset AND the rotation
         let midpoint:f64 = raster_size as f64 / 2.;
 
         // compute analytic FFT solution and modify <image>
         for n in 0..n_range {
-            // compute Ufac (and update the bessel functions)
+            // we compute the bessel function (depends only on n, not m)
             for index in 0..raster_size as usize {
-                bessel = Jn(n.into(), 2.*PI*coords[index].abs()*radius*scale);
-                Ufac[index] =  bessel * (Complex::new(0.0, n as f64) * (PI / 2. - phi_j) ).exp();
+                bessel[index] = Jn(n.into(), 2.*PI*coords[index].abs()*radius*scale);
             }
 
             for m in &m_vals {
-                z_line =  ( (n as f64) / ( pitch * scale ) + (*m as f64) / ( rise * scale) ).round() ;
-                // web_sys::console::log_1(&"N, and zline".into());
-                // web_sys::console::log_1(&n.into());
-                // web_sys::console::log_1(&z_line.into());
+                // we determine the layerline position
+                z_line = (n as f64) / ( pitch * scale ) + m / ( rise * scale);
+                z_draw_coord = z_line.round();  // get the rounded value, for plotting
+
+                phi_z =  2. * PI * z_line * offset * scale;
+                for index in 0..raster_size as usize {
+                    Ufac[index] =  bessel[index] * (Complex::new(0.0, (n as f64) * (PI / 2. - phi_j) + phi_z) ).exp();
+                }
+
+                //web_sys::console::log_1(&format!("N is {n}, M is {m}, and z_line is {z}", n=n, m=m, z=z_line).into());
+
                 // check if we are still within the limits of the rasterised image
-                if z_line > -(raster_size as f64)/2. && z_line < (raster_size as f64)/2. {
-                    let mut line =  image.slice_mut(s![(midpoint + z_line) as usize, ..]);
+                // if so, add the layerline amplitudes (Ufac)
+                if z_draw_coord > -(raster_size as f64)/2. && z_draw_coord < (raster_size as f64)/2. {
+                    let mut line =  image.slice_mut(s![(midpoint + z_draw_coord) as usize, ..]);
                     line += &ArrayView::from(&Ufac);
                     if n != 0{
-                        let mut line =  image.slice_mut(s![(midpoint - z_line) as usize, ..]);
+                        let mut line =  image.slice_mut(s![(midpoint - z_draw_coord) as usize, ..]);
                         line += &ArrayView::from(&Ufac);
                     }
                 }
@@ -155,9 +166,12 @@ pub fn FFT_analytic( helix_family: &JsValue, n_range: u8, m_range: u8, scale: f6
 
     // flatten the array
     let image_flat = image.into_shape((num_pix, 1)).unwrap();
-    // Intensity = |amplitude| ** 2
+    let normalisation = helices.len() as f64;  // ensure that more helices does not mean higher intensities
+
     let intensity = image_flat.mapv(
-        |amplitude| amplitude.norm().pow(2)*255.
+        //  we compute the intensity, which is |amplitude|^2.
+        //  We clip some values (>256), but it's probably best for now with limited dynamic range
+        |amplitude| (amplitude/normalisation).norm().pow(2) * 511.
     );
 
     let mut out:Vec<f64> = vec![0 as f64; 4 * num_pix];
